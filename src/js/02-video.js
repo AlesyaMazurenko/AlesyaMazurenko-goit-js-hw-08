d3358a57784c254c097d4c9558e4dd0a1a931eee
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-07-30 00:37:15 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,34 +4,48 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
+let getTimeFromStorage = '';
 console.log(player);
-
+playerStart();
  
 player.on('timeupdate', throttle(onTime, 1000));
     
-function onTime() {
-    player.getCurrentTime().then(function (seconds) {
-        localStorage.setItem("videoplayer-current-time", seconds);
-        console.log(seconds);
-         // seconds = the current playback position
-        }).catch(function(error) {
-          // an error occurred
-});
+function onTime(evt) {
+    const pleyedSeconds = evt.seconds;
+     localStorage.setItem("videoplayer-current-time", pleyedSeconds);
+    console.log(pleyedSeconds);
+    
+//  вариат 2
+    // player.getCurrentTime().then(function (seconds) {
+    //     localStorage.setItem("videoplayer-current-time", seconds);
+    //     console.log(seconds);
+    //      // seconds = the current playback position
+    //     }).catch(function(error) {
+    //       // an error occurred
+// });
 }
 
 // вклчаем воспроизведение начиная с последнего записанного времени
-const getTimeFromStorage = localStorage.getItem("videoplayer-current-time");
+function playerStart() {
+    getTimeFromStorage = localStorage.getItem("videoplayer-current-time");
+    if (getTimeFromStorage === '') { }
+    else { player.setCurrentTime(getTimeFromStorage); };
+};
+
+
+//  вариат 2
+// const getTimeFromStorage = localStorage.getItem("videoplayer-current-time");
  
-player.setCurrentTime(getTimeFromStorage).then(function(seconds) {  //функция взята из nmp
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-        default:
-            // some other error occurred
-            break;
-    }
-});
+// player.setCurrentTime(getTimeFromStorage).then(function(seconds) {  //функция взята из nmp
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
 
